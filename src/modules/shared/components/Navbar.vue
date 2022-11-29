297f5e32ab09b26188f8b5608c3ad97167f0d811
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-29 14:40:55 -0500

    Se crea componente para la barra de navegacion y se utiliza el componente router-link para la navegacion.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Inicio</router-link>
+            </li>
+            <li>
+                <router-link to="/id">Pokemon</router-link>
+            </li>
+            <li>
+                <router-link to="/about">Acerca de</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+ul {
+    padding: 0 30px;
+    list-style-type: none;
+}
+
+ul li {
+    display: inline;
+}
+
+li a {
+    padding: 8px;
+    background-color: #dddddd;
+}
+
+a.router-link-exact-active {
+    background-color: #cccccc;
+    color: #42b983;
+}
+</style>
