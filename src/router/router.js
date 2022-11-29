0f95218e1cab06f9a6ce1e31b4ab5a2239e92d36
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-29 11:53:13 -0500

    Se crea ruta para la pagina de error 404.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/router/router.js
+++ src/router/router.js
@@ -4,10 +4,13 @@ import List from '@/modules/pokemon/pages/List.vue'
 import About from '@/modules/pokemon/pages/About.vue'
 import Detail from '@/modules/pokemon/pages/Detail.vue'
 
+import PageNotFound from '@/modules/error/pages/PageNotFound.vue'
+
 const routes = [
     { path: '/', component: List },
     { path: '/about', component: About },
-    { path: '/:id', component: Detail },
+    { path: '/id', component: Detail },
+    { path: '/:pathMatch(.*)*', component: PageNotFound },
 ]
 
 const router = createRouter({
