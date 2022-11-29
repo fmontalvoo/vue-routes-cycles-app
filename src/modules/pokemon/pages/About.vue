fff3ccb8eecb28295754d9cd0afa0f826d6d0ee4
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-29 15:07:58 -0500

    Ciclo de vida de un componente en Vue.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/modules/pokemon/pages/About.vue
+++ src/modules/pokemon/pages/About.vue
@@ -1,10 +1,59 @@
 <template>
     <h2>About</h2>
+    <button @click="add">Counter: {{ count }}</button>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        add() {
+            this.count++
+        }
+    },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        console.log('created')
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate')
+    },
+    updated() {
+        console.log('updated')
+    },
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount')
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+    errorCaptured() {
+        console.log('errorCaptured')
+    },
+    renderTracked() {
+        console.log('renderTracked')
+    },
+    renderTriggered() {
+        console.log('renderTriggered')
+    },
 }
 </script>
 
